3a5ad13ba38f0918ff773c6dd9a86dfd73fce416
B1329002 <hana1007c@gmail.com>
2025-04-10 14:47:20 +0800

    Update main25.c

--- ex4/main25.c
+++ ex4/main25.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
-int multi(int i, int j) {
-    if (j == 1) {
-        printf("%d*%d=%d\n", i, j, i*j);
-        return 0;
-    } else {
-        multi(i, j-1);
-        ptintf("%d*%d=%d\n", i, j, i*j);
-    }
+#include <stdlib.h>
+
+int mulit(int i,int j){
+	if(i>9){
+		return;
+	}
+	if(j>9){
+		printf("\n");
+		mulit(i+1,1);
+	}else{
+		printf("%d*%d=%d\t",i,j,i*j);
+		mulit(i,j+1);
+	}
+	
 }
 
-int main() {
-    multi(1, 9);
-    return 0;
+int main(){
+	mulit(1,1);
+	
+	return 0;
 } 
